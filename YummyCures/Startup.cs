c49641cbc4689e54865885ff216fa0eb51bc460c
CeCe2065 <infinitewisdom2065@gmail.com>
2017-05-09 10:20:59 -0500

    Add project files.

---
+++ YummyCures/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YummyCures.Startup))]
+namespace YummyCures
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
